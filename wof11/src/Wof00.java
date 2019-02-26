535c37229371ecc6c5957a9171004d9d4a5fece9
Jan Janech <jan.janech@kst.uniza.sk>
2019-02-26 15:43:05 +0100

    koncepty pre dalsi vyvoj hry WoF

--- wof11/src/Wof00.java
+++ wof11/src/Wof00.java
@@ -5,7 +5,33 @@
  * @version 21.2.2012
  */
 public class Wof00 {
-
+/*
+    Mozne rozsirenia hry:
+    - Teleport
+    - Smrt
+        - Pad zo schodov s urcitou pravdepodobnostou
+    - Questy
+        - Ciel pre celu hru
+        - Odmena
+        - Vycistit podlahu v kazdej miestnosti
+    - Inventar
+        - Predmety
+        - Pivo (obnovi zivoty)
+        - Kluce od miestnosti
+        - Navleky
+        - Handra
+        - Cistiace prostriedky
+    - Levely/Charakteristiky
+        - Exp
+        - Zivoty - HP
+    - Npc
+        - Nepriatelia
+        - Bosovia
+        - Vratnicka
+        - Bufetarka
+    - Obchodovanie
+        - Peniaze
+*/
     /**
      * @param args parametre programu
      */
