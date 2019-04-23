7c7a40b5497a4a852aa33dfbd0f98648ad0e71d8
Jan Janech <jan.janech@kst.uniza.sk>
2019-04-23 10:34:20 +0200

    levia klietka

--- generickaKlietka/src/sk/uniza/fri/generickaklietka/GenerickaKlietka.java
+++ generickaKlietka/src/sk/uniza/fri/generickaklietka/GenerickaKlietka.java
@@ -16,6 +16,8 @@ public class GenerickaKlietka {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Klietka klietka = new Klietka();
+        klietka.vloz(new Lev());
+        System.out.println(klietka);
     }
-    
 }
---
+++ generickaKlietka/src/sk/uniza/fri/generickaklietka/Lev.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.uniza.fri.generickaklietka;
+
+/**
+ *
+ * @author janik
+ */
+public class Lev {
+
+    public Lev() {
+    }
+
+    @Override
+    public String toString() {
+        return "Lev{" + '}';
+    }
+    
+}
