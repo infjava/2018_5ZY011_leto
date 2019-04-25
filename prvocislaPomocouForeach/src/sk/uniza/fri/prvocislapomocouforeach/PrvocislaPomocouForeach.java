c909b87df02f02c3b8d503ad5aa96910fc541e66
Jan Janech <jan.janech@kst.uniza.sk>
2019-04-25 13:43:34 +0200

    uvod projektu prvocisla

---
+++ prvocislaPomocouForeach/src/sk/uniza/fri/prvocislapomocouforeach/PrvocislaPomocouForeach.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.uniza.fri.prvocislapomocouforeach;
+
+/**
+ *
+ * @author janik
+ */
+public class PrvocislaPomocouForeach {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
