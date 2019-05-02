e6a1416066bd8a157deb84bef654193b945f58de
Jan Janech <jan.janech@kst.uniza.sk>
2019-05-02 13:21:17 +0200

    novy projekt pre GUI zoznam studentov

---
+++ zoznamStudentovGUI/src/sk/uniza/fri/zoznamstudentov/ZoznamStudentovGUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.uniza.fri.zoznamstudentov;
+
+/**
+ *
+ * @author janik
+ */
+public class ZoznamStudentovGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
