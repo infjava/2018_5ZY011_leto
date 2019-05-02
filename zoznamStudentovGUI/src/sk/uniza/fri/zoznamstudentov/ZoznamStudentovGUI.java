683daf454273c044262fe2b1ff092bb6edd0f303
Jan Janech <jan.janech@kst.uniza.sk>
2019-05-02 13:25:57 +0200

    hlavne okno

--- zoznamStudentovGUI/src/sk/uniza/fri/zoznamstudentov/ZoznamStudentovGUI.java
+++ zoznamStudentovGUI/src/sk/uniza/fri/zoznamstudentov/ZoznamStudentovGUI.java
@@ -16,6 +16,8 @@ public class ZoznamStudentovGUI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        HlavneOkno okno = new HlavneOkno();
+        okno.setVisible(true);
     }
     
 }
